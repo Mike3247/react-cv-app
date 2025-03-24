993c99408354e3db187a21315ed70a912a96bf12
Mike3247 <Mike3247@github.com>
2025-03-24 19:57:31 -0400

    add automatic render of fullName text input value

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -1,5 +1,27 @@
-function GeneralInformation() {
-    return <h1>General Information</h1>
+import React, {useState, useEffect} from 'react';
+
+const GeneralInformation = (props) => {
+    const [value, setValue] = useState("");
+
+    return (
+        <div>
+        <h1>General Information</h1>
+        <h2>General Information Form</h2>
+        <form className="generalInformation" action="localhost:5173" method='post'>
+            <label for="fullName">Full Name:</label>
+            <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            placeholder="First and last name"
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+            />
+        </form>
+        <h1>{value}</h1>
+        </div>
+        
+    )
 }
 
 export default GeneralInformation;
